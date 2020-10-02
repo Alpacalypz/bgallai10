a1cf4aa2f14548937b4590e1e858bd8ef8d12dfb
Alpacalypz <bgallai10@stuy.edu>
2020-10-02 00:10:50 -0400

    added K06

---
+++ 06_05-rf/K06.py
@@ -0,0 +1,12 @@
+import random
+
+KREWES = {
+    'orpheus': ['ERIC', 'SAUVE', 'JONATHAN', 'PAK', 'LIAM', 'WINNIE', 'KELLY', 'JEFFREY', 'KARL', 'ISHITA', 'VICTORIA', 'BENJAMIN', 'ARIB', 'AMELIA', 'CONSTANCE', 'IAN'],
+    'rex': ['ANYA', 'DUB-Y', 'JESSICA', 'ALVIN', 'HELENA', 'MICHELLE', 'SHENKER', 'ARI', 'STELLA', 'RENEE', 'MADELYN', 'MAC', 'RYAN', 'DRAGOS'],
+    'endymion': ['JASON', 'DEAN', 'MADDY', 'SAQIF', 'CINDY', 'YI LING', 'RUOSHUI', 'FB', 'MATTHEW', 'MAY', 'ERIN', 'MEIRU']
+}
+
+team = input("Pick a team from orpheus, rex, or endymion: ").lower()
+while team not in KREWES:
+    team = input("Please enter either orpheus, rex, or endymion: ").lower()
+print(random.choice(KREWES[team]))
